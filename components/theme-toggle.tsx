2d5e43eeab154ec1c3abfcaae60a8c9e0cc1a291
adnanexpertizo <adnan.expertizo@gmail.com>
2025-08-30 23:43:28 +0500

    fix

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -27,17 +27,17 @@ const ThemeToggle = () => {
       <div className="w-16 h-8 bg-muted border-2 border-border rounded-full flex items-center p-[2px] transition-all duration-300 ease-out group-hover:shadow-md group-hover:border-primary/30">
         <Sun
           className={`w-4 h-4 relative z-30 ml-1 transition-all duration-300 ${
-            theme === "dark" ? "text-muted-foreground opacity-40" : "text-primary opacity-100"
+            theme === "dark" ? "  text-foreground" : "bg-primary text-white opacity-100"
           }`}
         />
         <Moon
           className={`w-4 h-4 z-30 mr-1 ml-auto transition-all duration-300 ${
-            theme === "dark" ? "text-primary opacity-100" : "text-muted-foreground opacity-40"
+            theme === "dark" ? "bg-primary opacity-100 text-white" : " text-foreground "
           }`}
         />
         <span
-          className={`absolute w-6 h-6 bg-primary rounded-full shadow-md transition-all duration-300 ease-out ${
-            theme !== "dark" ? "translate-x-[2px]" : "translate-x-[34px]"
+          className={`absolute duration-1000 w-6 h-6 bg-primary rounded-full shadow-md transition-all duration ease-out ${
+            theme !== "dark" ? "translate-x-[1px]" : "translate-x-[33px]"
           } group-hover:shadow-lg`}
         />
       </div>
