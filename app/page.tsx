8008f8c2db81c5871573017cc3a438fe2a04421d
adnanexpertizo <adnan.expertizo@gmail.com>
2025-08-30 23:02:04 +0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { Footer } from "@/components/footer"
 export default function Home() {
   return (
     <main className="min-h-screen ">
-      <div className="lg:px-24 md:px-16 px-4 mx-auto">
+      <div className="lg:px-24 md:px-16 px-2 mx-auto">
       <Navbar />
       <HeroSection />
       <AboutSection />
